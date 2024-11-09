c407ec5980d278963725bb0082b9ea88568dc903
PQ-XY <yaoxu369@gmail.com>
2024-11-09 10:36:48 -0500

    1st update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Meals from './Components/Meals';
+import Planner from './Components/Planner';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App></App>}>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/meals' element={<Meals></Meals>}></Route>
+          <Route path='/planner' element={<Planner></Planner>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
